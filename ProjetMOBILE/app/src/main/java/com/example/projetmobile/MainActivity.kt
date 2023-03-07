753ba21da9111d27f9e9bb11494d388c01e18ee2
Bastien8 <bastien.vernieres@isen.yncrea.fr>
2023-03-07 16:47:25 +0100

    truc quiu marche ap

--- ProjetMOBILE/app/src/main/java/com/example/projetmobile/MainActivity.kt
+++ ProjetMOBILE/app/src/main/java/com/example/projetmobile/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projetmobile.databinding.ActivityMainBinding
 import com.google.firebase.database.*
@@ -19,6 +20,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
 
+    private fun buttonListener() {
+        binding.CreaCompte.setOnClickListener {
+            Log.d("CreaCompte", "Click sur le button Créer un compte")
+            Toast.makeText(this, "j'ai cliqué", Toast.LENGTH_LONG).show()
+        }
+    }
+
     companion object {
         private const val TAG = "KotlinActivity"
     }
@@ -31,59 +39,42 @@ class MainActivity : AppCompatActivity() {
             startActivity(intentToInscription)
         }
 
-        ReadAndWriteSnippets().initializeDbRef()
+        //ReadAndWriteSnippets().initializeDbRef()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val uid = UUID.randomUUID().toString()
-        Log.d("TAG",uid)
+        //val uid = UUID.randomUUID().toString()
+        //Log.d("TAG",uid)
         //ReadAndWriteSnippets().initializeDbRef()
         //database = FirebaseDatabase.getInstance().getReference("User")
-        ReadAndWriteSnippets().writeNewUser(uid,"Spring","clem83spring@gmail.com")
-        createReservation()
+        //ReadAndWriteSnippets().writeNewUser(uid,"Spring","clem83spring@gmail.com")
+        //createReservation()
     }
 
 
 
-    fun createReservation() {
-        val df = SimpleDateFormat("yyyy-MM-dd", Locale.FRANCE)
-        val date = df.format(Date())
-
-        val hours = mapOf("12" to "4bf7d5a9-484a-4aed-8c72-431254b2710e")
-        val reservation = Reservation(date, hours)
-
-        DataBaseHelper.database.child("Reservations").child(reservation.date).setValue(reservation)
+    private fun switchPage(){
+        val intent = Intent(this,inscription ::class.java)
+        intent.putExtra(inscription.extraKey, TAG)
+        startActivity(intent)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d( "lifeCycle", "MainActivity onStart")
     }
 
-    @IgnoreExtraProperties
-    data class Reservation(val date: String, val hours: Map<String, String>) {
-        // Null default values create a no-argument default constructor, which is needed
-        // for deserialization from a DataSnapshot.
+    override fun onResume() {
+        super.onResume()
+        Log.d( "lifeCycle", "MainActivity onResume")
     }
 
-    fun basicReadWrite() {
-        // [START write_message]
-        // Write a message to the database
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+    override fun onPause() {
+        super.onPause()
+        Log.d("lifeCycle", "MainActivity onPause")
+    }
 
-        myRef.setValue("Hello, World!")
-        // [END write_message]
-
-        // [START read_message]
-        // Read from the database
-        myRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = dataSnapshot.getValue<String>()
-                Log.d(TAG, "Value is: $value")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
+    override fun onDestroy() {
+        Log.d("lifeCycle", "MainActivity onDestroy")
+        super.onDestroy()
     }
 
 }
--- ProjetMOBILE/app/src/main/java/com/example/projetmobile/inscription.kt
+++ ProjetMOBILE/app/src/main/java/com/example/projetmobile/inscription.kt
@@ -2,11 +2,19 @@ package com.example.projetmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.projetmobile.databinding.ActivityInscriptionBinding
 
 class inscription : AppCompatActivity() {
+    private lateinit var binding: ActivityInscriptionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inscription)
+        binding = ActivityInscriptionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+    }
+
+    companion object{
+        val extraKey = "extraKey"
     }
 }
